9b997f05f3cc77ccc019b36869ad057b86bb717b
JAVA <JAVA@DQX-LABA02>
2014-02-27 09:36:11 +0000

    teste

--- ProjImobiliaria/src/control/Programa.java
+++ ProjImobiliaria/src/control/Programa.java
@@ -4,7 +4,6 @@ public class Programa {
 
 	/**
 	 * vamos apagar esta classe para criar o model depois
-	 * é só terminarmos o diagrama de classes
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub teste
