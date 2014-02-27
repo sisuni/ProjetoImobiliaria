a2898d35be75e6c6f5d04ed055a28626b4fc6d95
Valdecir <Valdecir@DellStation>
2014-02-26 23:18:12 -0300

    Comit inicial de configuração

---
+++ ProjImobiliaria/src/control/Programa.java
@@ -0,0 +1,13 @@
+package control;
+
+public class Programa {
+
+	/**
+	 * vamos apagar esta classe para criar o model depois
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub teste
+
+	}
+
+}
