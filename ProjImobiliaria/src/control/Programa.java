6ecf3fca394c7eac0289d8ed7e1522e1f06b94d1
Sistemas Unigranrio <sisuni@outlook.com>
2014-03-10 11:49:06 -0300

    Apagando a alteração do comentário online
    Só para ensiar o Dérick a funcionalidade do Github online

--- ProjImobiliaria/src/control/Programa.java
+++ ProjImobiliaria/src/control/Programa.java
@@ -4,7 +4,6 @@ public class Programa {
 
 	/**
 	 * vamos apagar esta classe para criar o model depois
-	 * ensinando ao Dérick 
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub teste
