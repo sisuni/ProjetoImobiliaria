74a9d7bf6dec2a569a0856a8da2701729899bf00
JAVA <JAVA@DQX-LABB16>
2014-03-10 11:46:27 -0300

    Aqui eu só mechi no comentário pra poder ensinar o Dérick como usar o Github

--- ProjImobiliaria/src/control/Programa.java
+++ ProjImobiliaria/src/control/Programa.java
@@ -4,6 +4,7 @@ public class Programa {
 
 	/**
 	 * vamos apagar esta classe para criar o model depois
+	 * ensinando ao Dérick 
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub teste
