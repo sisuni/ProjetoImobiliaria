5059286c90324da13dd71cfab484cd7649be49c4
JAVA <JAVA@DQX-LABB16>
2014-03-10 11:55:40 -0300

    Foi criado pacote model com a classe teste e implementei.

---
+++ ProjImobiliaria/src/model/teste.java
@@ -0,0 +1,20 @@
+package model;
+
+public class teste {
+	private String nome;
+
+	public teste(String nome) {
+		super();
+		this.nome = nome;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
+	
+}
