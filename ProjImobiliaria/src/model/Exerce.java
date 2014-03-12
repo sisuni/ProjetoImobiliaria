184380e534548b39cc0cee891db26e788d1813fb
matheus.castro <matheus.castro@localhost>
2014-03-12 16:21:28 -0300

    implementando a primeira parte do projeto, com as classes Pessoa, Cliente, Funcionario, Cargo e Exerce.

--- ProjImobiliaria/src/model/Exerce.java
+++ ProjImobiliaria/src/model/Exerce.java
@@ -7,4 +7,13 @@ public class Exerce {
 	public Exerce(String funcao){
 		this.funcao = funcao;
 	}
+
+	public String getFuncao() {
+		return funcao;
+	}
+
+	public void setFuncao(String funcao) {
+		this.funcao = funcao;
+	}
+	
 }
