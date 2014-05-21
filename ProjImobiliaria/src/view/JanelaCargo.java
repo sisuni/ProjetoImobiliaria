ab519cd9dbb5f7dec50b666bbf07799ca9770eae
matheus.castro <mthcastro@me.com>
2014-05-21 13:08:11 +0100

    Criação da classe CtrlManterCargo e da JanelaCargo

---
+++ ProjImobiliaria/src/view/JanelaCargo.java
@@ -0,0 +1,16 @@
+package view;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+import control.CtrlManterCargo;
+
+public class JanelaCargo extends JFrame{
+	private JPanel contentPane;
+	private CtrlManterCargo ctrlManter;
+	
+	public JanelaCargo(CtrlManterCargo c){
+		
+	}
+
+}
