b3b1f97894da5781f49ca27be58363a37f807d46
Matheus Castro <mthcastro@me.com>
2014-06-09 23:37:27 -0300

    Atualizando a viewer JanelaSalvaProprietario, contendo uma table para inclusão de telefones

--- ProjImobiliaria/src/view/JanelaSalvaProprietario.java
+++ ProjImobiliaria/src/view/JanelaSalvaProprietario.java
@@ -1,6 +1,5 @@
 package view;
 
-import java.awt.Component;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.text.ParseException;
@@ -12,18 +11,28 @@ import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
 import javax.swing.JTextField;
+import javax.swing.SwingConstants;
 import javax.swing.border.EmptyBorder;
+import javax.swing.event.TableModelEvent;
+import javax.swing.event.TableModelListener;
+import javax.swing.table.DefaultTableCellRenderer;
+import javax.swing.table.DefaultTableModel;
+import javax.swing.table.TableModel;
 import javax.swing.text.MaskFormatter;
 
 import model.ModelException;
-import control.ICtrlManter;
 import control.ICtrlManterProprietarios;
+import control.ICtrlManterTelefones;
+import control.ITabelavel;
 
 public class JanelaSalvaProprietario extends JFrame implements
 		IViewerSalvaProprietario {
 
 	private ICtrlManterProprietarios ctrl;
+	private ICtrlManterTelefones ctrlTel;
 
 	private boolean ehAlteracao;
 
@@ -32,15 +41,16 @@ public class JanelaSalvaProprietario extends JFrame implements
 	private JFormattedTextField txtCpf;
 	private JTextField txtEmail;
 	private JTextField txtEnd;
-	private JComboBox cmbBanco;
+	private JComboBox<String> cmbBanco;
 	private JTextField txtConta;
 	private JTextField txtAgencia;
+	private JTable tableNum;
 
 	public JanelaSalvaProprietario(ICtrlManterProprietarios sc) throws ParseException {
 		this.ctrl = sc;
 		setTitle("Salva Proprietario - Imobiliária");
 		setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
-		setBounds(100, 100, 420, 320);
+		setBounds(100, 100, 420, 520);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
@@ -58,7 +68,7 @@ public class JanelaSalvaProprietario extends JFrame implements
 		contentPane.add(lblNome);
 
 		txtNome = new JTextField();
-		txtNome.setBounds(72, 41, 334, 20);
+		txtNome.setBounds(72, 41, 330, 20);
 		contentPane.add(txtNome);
 
 		JLabel lblCpf = new JLabel("CPF:");
@@ -74,7 +84,7 @@ public class JanelaSalvaProprietario extends JFrame implements
 		contentPane.add(lblEmail);
 
 		txtEmail = new JTextField();
-		txtEmail.setBounds(220, 71, 185, 20);
+		txtEmail.setBounds(220, 71, 181, 20);
 		contentPane.add(txtEmail);
 
 		JLabel lblEnd = new JLabel("Endereço:");
@@ -82,7 +92,7 @@ public class JanelaSalvaProprietario extends JFrame implements
 		contentPane.add(lblEnd);
 
 		txtEnd = new JTextField();
-		txtEnd.setBounds(72, 103, 334, 20);
+		txtEnd.setBounds(72, 103, 330, 20);
 		contentPane.add(txtEnd);
 
 		JLabel lblDadosP = new JLabel("Dados Financeiros");
@@ -101,7 +111,7 @@ public class JanelaSalvaProprietario extends JFrame implements
 		contentPane.add(lblAgencia);
 
 		txtAgencia = new JTextField();
-		txtAgencia.setBounds(72, 210, 100, 20);
+		txtAgencia.setBounds(72, 208, 100, 20);
 		contentPane.add(txtAgencia);
 
 		JLabel lblConta = new JLabel("Conta:");
@@ -109,16 +119,86 @@ public class JanelaSalvaProprietario extends JFrame implements
 		contentPane.add(lblConta);
 
 		txtConta = new JTextField();
-		txtConta.setBounds(220, 210, 100, 20);
+		txtConta.setBounds(220, 208, 100, 20);
 		contentPane.add(txtConta);
 		
+		JLabel lblTel = new JLabel("Telefones");
+		lblTel.setBounds(20, 245, 150, 14);
+		lblTel.setFont(lblTel.getFont().deriveFont(16.0f));
+		contentPane.add(lblTel);
+		
+		/*Inicio da Table*/
+		JScrollPane scrollPane = new JScrollPane();
+		scrollPane.setBounds(21, 270, 380, 120);
+		contentPane.add(scrollPane);
+		
+		tableNum = new JTable();
+		DefaultTableCellRenderer renderer = (DefaultTableCellRenderer) tableNum.getDefaultRenderer(JLabel.class);  
+        renderer.setHorizontalAlignment(SwingConstants.CENTER); 
+        tableNum.setModel(new DefaultTableModel(
+			new Object[][] {
+			},
+			new String[] {
+				"Tipo", "Número"
+			}
+		));
+        tableNum.getColumnModel().getColumn(0).setPreferredWidth(60);
+        tableNum.getColumnModel().getColumn(1).setPreferredWidth(220);
+			
+     // monitorando o evento através de TableModelListener
+		tableNum.getModel().addTableModelListener(new TableModelListener() {
+		    public void tableChanged(TableModelEvent e) {
+		    	tratarModificacaoNaTabela(e);
+		    }
+		});
+		
+		scrollPane.setViewportView(tableNum);
+		this.setVisible(true);
+		/*Fim da Table*/
+				
+		/*Inicio dos Botões Telefone*/
+		JButton btnIncluir = new JButton("Incluir");
+		btnIncluir.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				executarIncluir();
+			}
+		});
+		btnIncluir.setBounds(60, 395, 89, 23);
+		btnIncluir.setToolTipText("Incluir Novo Telefone");
+		contentPane.add(btnIncluir);
+			
+		JButton btnAlterar = new JButton("Alterar");
+		btnAlterar.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				executarAlterar();
+			}
+		});
+		btnAlterar.setBounds(162, 395, 89, 23);
+		btnAlterar.setToolTipText("Alterar Telefone Selecionado");
+		contentPane.add(btnAlterar);
+		
+		JButton btnExcluir = new JButton("Excluir");
+		btnExcluir.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				executarExcluir();
+			}
+		});
+		btnExcluir.setBounds(263, 395, 89, 23);
+		btnExcluir.setToolTipText("Excluir Telefone Selecionado");
+		contentPane.add(btnExcluir);
+		
+		/*Fim dos Botões Telefone*/
+		
+		
+		/*Inicio dos Botões Salva Proprietario*/
 		JButton btnSalvar = new JButton("Salvar");
 		btnSalvar.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
 				executarSalvar();
 			}
 		});
-		btnSalvar.setBounds(53, 255, 143, 23);
+		btnSalvar.setBounds(53, 455, 143, 23);
+		btnSalvar.setToolTipText("Salvar Proprietário");
 		contentPane.add(btnSalvar);
 
 		JButton btnCancelar = new JButton("Cancelar");
@@ -127,12 +207,56 @@ public class JanelaSalvaProprietario extends JFrame implements
 				executarCancelar();
 			}
 		});
-		btnCancelar.setBounds(216, 255, 154, 23);
+		btnCancelar.setBounds(216, 455, 154, 23);
+		btnCancelar.setToolTipText("Cancelar");
 		contentPane.add(btnCancelar);
-
+		
+		/*Fim dos Botões Salva Proprietario*/
+		
 		this.setVisible(true);
 	}
-
+	
+	/* Inicio dos metódos pertencentes ao telefone*/
+	public void limpar() {
+		DefaultTableModel dtm = (DefaultTableModel)this.tableNum.getModel();
+		while(dtm.getRowCount() > 0)
+			dtm.removeRow(0);
+	}
+	
+	public void incluirLinha(ITabelavel objeto) {
+		DefaultTableModel dtm = (DefaultTableModel)this.tableNum.getModel();
+		dtm.addRow(objeto.getData());
+	}
+	
+	public void executarIncluir() {
+		this.ctrl.iniciarIncluir();
+	}
+	
+	public void executarExcluir() {
+		// Recupero a posição selecionada
+		int pos = tableNum.getSelectedRow();
+		// Se a posição for -1, não há ninguém selecionado. Então cancelo
+		// a operação
+		if(pos < 0)
+			return;
+		// Informo ao controlador para iniciar o processo de exclusão
+		ctrl.iniciarExcluir(pos);	
+	}
+	
+	public void executarAlterar() {
+		// Recupero a posição selecionada
+		int pos = tableNum.getSelectedRow();
+		// Se a posição for -1, não há ninguém selecionado. Então cancelo
+		// a operação
+		if(pos < 0)
+			return;
+		// Informo ao controlador para iniciar o processo de alteração
+		ctrl.iniciarAlterar(pos);	
+	}
+	
+	/* Fim dos metódos pertencentes ao telefone*/
+	
+	/*Método para preencher a lista de 124 bancos na combo*/
 	public void preencherBancos(){
 		String[] listaBancos = {"Banco ABC Brasil S.A.","Banco ABN AMRO S.A.","Banco Alfa S.A.","Banco Alvorada S.A.","Banco Banerj S.A.","Banco Bankpar S.A.",
 				"Banco Barclays S.A.","Banco BBM S.A.","Banco Beg S.A.","Banco BGN S.A.","Banco BM&FBOVESPA de Serviços de Liquidação e Custódia S.A","Banco BMG S.A.",
@@ -151,8 +275,8 @@ public class JanelaSalvaProprietario extends JFrame implements
 				"BPN Brasil Banco Múltiplo S.A.","Brasil Plural S.A. - Banco Múltiplo","BRB - Banco de Brasília S.A.","Caixa Econômica Federal","Citibank S.A.","Concórdia Banco S.A.","Deutsche Bank S.A. - Banco Alemão","Goldman Sachs do Brasil Banco Múltiplo S.A.","Hipercard Banco Múltiplo S.A.",
 				"HSBC Bank Brasil S.A. - Banco Múltiplo","ING Bank N.V.","Itaú UniBanco Holding S.A.","Itaú UniBanco S.A.","JPMorgan Chase Bank","Scotiabank Brasil S.A. Banco Múltiplo","Standard Chartered Bank (Brasil) S/A–Bco Invest.","UBS Brasil Banco de Investimento S.A.","UNIBANCO - União de Bancos Brasileiros S.A.",
 				"Unicard Banco Múltiplo S.A."};
-		cmbBanco = new JComboBox(listaBancos);
-		cmbBanco.setBounds(72, 172, 334, 18);
+		cmbBanco = new JComboBox<String>(listaBancos);
+		cmbBanco.setBounds(72, 172, 330, 18);
 		contentPane.add(cmbBanco);
 	}
 	
@@ -200,6 +324,18 @@ public class JanelaSalvaProprietario extends JFrame implements
 		this.txtConta.setText(conta);
 		this.ehAlteracao = true;
 
+	}
+	
+	public void tratarModificacaoNaTabela(TableModelEvent e) {
+        if(e.getType() != TableModelEvent.UPDATE)
+        	return;
+		int linha = e.getFirstRow();
+        int coluna = e.getColumn();
+ 
+        TableModel model = (TableModel) e.getSource();
+ 
+        System.out.println("Você alterou a linha " + linha + ", coluna " + coluna);
+        System.out.println("Valor da célula alterada: " + model.getValueAt(linha, coluna));
 	}
 
 }
